8025f8439fa41ffb6b36762dfd0a6715ab104a26
davidmise <65760058+davidmise@users.noreply.github.com>
2023-12-23 13:20:38 +0300

    modified the gallery

--- src/components/GallerySection.vue
+++ src/components/GallerySection.vue
@@ -16,7 +16,7 @@
         <div class=" container mx-auto row">
             <!-- first row -->
             <div class="row">
-                <div data-aos="zoom-in-up"  class="col-lg-6 justify-content-center">
+                <div data-aos="zoom-in-up"  class="col-lg-6 col-md-6 justify-content-center">
                     <img
                         src="../assets/images/grid/9.jpg"
                         class="w-100  shadow-1-strong rounded mb-4"
@@ -24,7 +24,7 @@
                     />
                 </div>
 
-                <div data-aos="zoom-in-up"  class="col-lg-6">
+                <div data-aos="zoom-in-up"  class="col-lg-6 col-md-6">
                     <img
                         src="../assets/images/grid/2.jpg"
                         class="w-100 shadow-1-strong rounded mb-4"
@@ -35,7 +35,7 @@
                 
             <!-- second row styarts here  -->
             <div  class="row">
-                <div data-aos="zoom-in-up"  class="col-lg-6">
+                <div data-aos="zoom-in-up"  class="col-lg-6 col-md-6">
                     <img
                         src="../assets/images/grid/3.jpg"
                         class="w-100 shadow-1-strong rounded mb-4"
@@ -43,12 +43,12 @@
                     />
                 </div>
                 <!-- first half ends here-->
-                <div class=" col-lg-6">
+                <div class="col-lg-6 col-md-6 ">
                     <!-- first row -->
                     <div class="row">
-                        <div data-aos="zoom-in-up"  class="col-lg-6">
+                        <div data-aos="zoom-in-up"  class="col-lg-6 col-md-6 ">
                             <div class="row">
-                                <div data-aos="zoom-in-up"  class="col-lg-12">
+                                <div data-aos="zoom-in-up"  class="col-lg-12 col-md-12">
                                     <img
                                         src="../assets/images/grid/4.jpg"
                                         class="w-100  shadow-1-strong rounded mb-4"
@@ -57,7 +57,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div data-aos="zoom-in-up"  class="col-lg-12">
+                                <div data-aos="zoom-in-up"  class="col-lg-12 col-md-12 ">
                                     <img
                                         src="../assets/images/grid/10.jpg"
                                         class="w-100  shadow-1-strong rounded mb-4"
@@ -67,9 +67,9 @@
                             </div>
                            
                         </div>
-                        <div data-aos="zoom-in-up"  class="col-lg-6">
+                        <div data-aos="zoom-in-up"  class="col-lg-6 col-md-6">
                             <div class="row">
-                                <div data-aos="zoom-in-up"  class="col-lg-12">
+                                <div data-aos="zoom-in-up"  class="col-lg-12 col-md-12">
                                     <img
                                         src="../assets/images/grid/5.jpg"
                                         class="w-100  shadow-1-strong rounded mb-4"
@@ -78,7 +78,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div data-aos="zoom-in-up"  class="col-lg-12">
+                                <div data-aos="zoom-in-up"  class="col-lg-12 col-md-12">
                                     <img
                                         src="../assets/images/grid/6.jpg"
                                         class="w-100  shadow-1-strong rounded mb-4"
