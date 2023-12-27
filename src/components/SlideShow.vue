31819f6f872b5e4a78a08331bcd6076775f0ca54
davidmise <davidkingmushi@gmail.com>
2023-12-27 10:01:43 +0300

    modified  the slideshow to  be responsive

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,24 +1,25 @@
 <template>
-    <div id="carouselExampleControls" class="carousel slide  shadow-lg carousel-fade " data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="4" aria-label="Slide 5"></button>
-            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="5" aria-label="Slide 6"></button>
-        </div>
-        <div class="carousel-inner">     
+    <section class="slideshow">
+        <div id="carouselExampleControls" class=" slideshow carousel slide  shadow-lg carousel-fade " data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            </div>
+            <div class="carousel-inner">     
             <div v-for="(image, index) in images" :key="index" :class="['carousel-item slide ', index === 0 ? 'active' : '']">
-                <img :src="image" class=" d-block w-100"  alt="Slide" />
+                <img :src="image" class=" d-block w-100 slideshow"  alt="Slide" />
                 <div class="top">
-                    
+                 
                 </div>
                     <div  class="centered ">
                         <div class="row text-center text-white">
-                            <div class="col-lg-12 mx-auto">
-                                <strong class="fw-bold vw-4"> Creative thinking.  </strong> <br>
-                                    <span class="vw-3"> 
+                            <div class="col-lg-12  ">
+                                <strong class="fw-bold vw-4 sm-txt-4"> Creative Thinking.  </strong> <br>
+                                    <span class="vw-3 sm-txt-3"> 
                                         Introducing new ideas or methods  
                                         Characteristics of One Person or thing, and 
                                         so serving to distinguish it from others.
@@ -37,6 +38,8 @@
             <span class="visually-hidden" >Next</span>
         </button>
     </div>
+    </section>
+   
 </template>
 <script>
     import img1 from '../assets/images/carousel/1.jpg';
@@ -52,3 +55,21 @@
         },
     }
 </script>
+<style>
+/* Hide the navigation links on small devices by default */
+@media (max-width: 991.98px) {
+ .slideshow{
+    height: 100vh;
+    background: #000;
+ }
+ .sm-txt-3{
+    font-size: 3vh;
+
+ }
+ .sm-txt-4{
+    font-size: 4vh;
+
+ }
+ 
+}
+</style>
